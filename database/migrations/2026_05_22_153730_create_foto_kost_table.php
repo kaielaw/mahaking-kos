18e992840676ee71c804da014d1faeeb9c606a3e
azahwaen <azahwaezarmei@gmail.com>
2026-05-22 22:54:11 +0700

    sudone

---
+++ database/migrations/2026_05_22_153730_create_foto_kost_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('foto_kost', function (Blueprint $table) {
+
+            $table->string('id_foto')->primary();
+
+            $table->string('id_kost');
+
+            $table->string('foto_kost');
+
+            $table->text('deskripsi_foto')->nullable();
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('foto_kost');
+    }
+};
